8f106d419703b55149dc7fdda8c84fa83357b080
ronke11 <rsoyekunle@gmail.com>
2019-06-21 21:34:02 -0700

    Methods exercises

---
+++ methods/methods.rb
@@ -0,0 +1,20 @@
+def greeting(name)
+  puts "Hello " + name + "!"
+end
+
+greeting("James")
+
+
+def multiply(a,b)
+  a * b
+end
+
+puts multiply(718,9313)
+
+
+def scream(words)
+  words = words + "!!!!"
+  puts words
+end 
+
+scream("Yippee")
